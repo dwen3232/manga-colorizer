038a7afa74c34275d2a85bd59a64c7bb54af17ea
David Wen <davidrwen@gmail.com>
2024-12-28 20:15:30 -0500

    test: for convenience

--- tests/manga_colorizer/processors/test_panel_cleaner.py
+++ tests/manga_colorizer/processors/test_panel_cleaner.py
@@ -1,8 +1,27 @@
+from pathlib import Path
+
 from manga_colorizer.processors.panel_cleaner import PanelCleaner
 
+color_path = Path("./manga-raw/color1/")
+color_img_paths = [
+    path
+    for chapter_path in color_path.iterdir()
+    if chapter_path.is_dir()
+    if chapter_path.is_dir() and "1001" in chapter_path.name
+    for path in chapter_path.iterdir()
+]
 
-def test():
+
+def test_display_ink_images():
+    ink_path = Path("./manga-raw/ink1/")
+    ink_img_paths = [
+        path
+        for chapter_path in ink_path.iterdir()
+        if chapter_path.is_dir()
+        if chapter_path.is_dir() and "1001" in chapter_path.name
+        for path in chapter_path.iterdir()
+    ]
     cleaner = PanelCleaner()
-    cleaner.display(
-        "/Users/davidwen/Repositories/manga-colorizer/manga-raw/color1/manga_one-piece-digital-colored-chapter-1001/03.webp"
-    )
+
+    for path in ink_img_paths:
+        cleaner.display(path)
